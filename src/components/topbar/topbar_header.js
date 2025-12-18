09c3bb2a28f46a8442fabd2a37d41deae5c1bae2
Nguyễn Đình Trưng <nguyendinhtrung.it@gmail.com>
2025-12-18 23:10:26 +0700

    fix header

--- src/components/topbar/topbar_header.js
+++ src/components/topbar/topbar_header.js
@@ -1,6 +1,5 @@
-import { ArrowRight, ChevronDown, CircleUserRound, Heart, RefreshCcw, ShoppingCart } from 'lucide-react';
+import { ChevronDown } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import { IMAGES } from '../../constants/images/image';
 
 const TopBar_Header = () => {
   return (
@@ -22,48 +21,6 @@ const TopBar_Header = () => {
           <p className="text-xs">Need help? Call Us: <span className="text-primary font-medium border-r border-gray-300 pr-2">+889.541.507</span></p>
           <p className="flex items-center text-xs cursor-pointer border-r border-gray-300 pr-2">English <ChevronDown size={10} className="ml-1" /></p>
           <p className="flex items-center text-xs cursor-pointer">INR <ChevronDown size={10} className="ml-1" /></p>
-        </div>
-      </div>
-      {/* Header */}
-      <div className="flex justify-between items-center p-4 px-24">
-        <img src={IMAGES.logo} alt="Header Banner" className="w-24" />
-        <div className="flex justify-between border border-primary rounded-sm p-1 w-1/3">
-          <input type="text" placeholder="Search for products..." className="px-4 text-xs w-full focus:outline-none" />
-          <button className="bg-primary text-white px-4 py-2 rounded-sm text-xs hover:bg-primaryDark transition-colors">Search</button>
-        </div>
-        <div className="flex items-center space-x-6">
-          <button className="flex border border-primary text-primary px-4 py-2 rounded-sm text-xs hover:bg-primaryDark transition-colors hover:text-white">Became Vendor <ArrowRight size={16} className="ml-1" /></button>
-          {/* Compare with badge */}
-          <div className="flex items-center cursor-pointer transition-colors relative group">
-            <div className="relative">
-              <RefreshCcw size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
-              <span className="absolute -top-2 -right-2 bg-primary text-white text-[10px] font-bold rounded-full w-4 h-4 flex items-center justify-center">3</span>
-            </div>
-            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Compare</p>
-          </div>
-
-          {/* Wishlist with badge */}
-          <div className="flex items-center cursor-pointer transition-colors relative group">
-            <div className="relative">
-              <Heart size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
-              <span className="absolute -top-2 -right-2 bg-primary text-white text-[10px] font-bold rounded-full w-4 h-4 flex items-center justify-center">5</span>
-            </div>
-            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Wishlist</p>
-          </div>
-
-          {/* Cart with badge */}
-          <div className="flex items-center cursor-pointer transition-colors relative group">
-            <div className="relative">
-              <ShoppingCart size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
-              <span className="absolute -top-2 -right-2 bg-primary text-white text-[10px] font-bold rounded-full w-4 h-4 flex items-center justify-center">2</span>
-            </div>
-            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Cart</p>
-          </div>
-
-          <div className="flex items-center cursor-pointer transition-colors group">
-            <CircleUserRound size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
-            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Account</p>
-          </div>
         </div>
       </div>
     </div>
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,10 +1,58 @@
+import { ArrowRight, CircleUserRound, Heart, RefreshCcw, ShoppingCart } from 'lucide-react';
+import Navbar from '../../components/navbar/Navbar';
 import TopBar_Header from '../../components/topbar/topbar_header';
+import { IMAGES } from '../../constants/images/image';
+
 
 const HomePage = () => {
   return (
     <div>
-      {/* Header */}
+      {/* TopHeader */}
       <TopBar_Header />
+      {/* Header */}
+      <div className="flex justify-between items-center p-4 px-24">
+        <img src={IMAGES.logo} alt="Header Banner" className="w-24" />
+        <div className="flex justify-between border border-primary rounded-sm p-1 w-1/3">
+          <input type="text" placeholder="Search for products..." className="px-4 text-xs w-full focus:outline-none" />
+          <button className="bg-primary text-white px-4 py-2 rounded-sm text-xs hover:bg-primaryDark transition-colors">Search</button>
+        </div>
+        <div className="flex items-center space-x-6">
+          <button className="flex border border-primary text-primary px-4 py-2 rounded-sm text-xs hover:bg-primaryDark transition-colors hover:text-white">Became Vendor <ArrowRight size={16} className="ml-1" /></button>
+          {/* Compare with badge */}
+          <div className="flex items-center cursor-pointer transition-colors relative group">
+            <div className="relative">
+              <RefreshCcw size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
+              <span className="absolute -top-2 -right-2 bg-primary text-white text-[10px] font-bold rounded-full w-4 h-4 flex items-center justify-center">3</span>
+            </div>
+            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Compare</p>
+          </div>
+
+          {/* Wishlist with badge */}
+          <div className="flex items-center cursor-pointer transition-colors relative group">
+            <div className="relative">
+              <Heart size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
+              <span className="absolute -top-2 -right-2 bg-primary text-white text-[10px] font-bold rounded-full w-4 h-4 flex items-center justify-center">5</span>
+            </div>
+            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Wishlist</p>
+          </div>
+
+          {/* Cart with badge */}
+          <div className="flex items-center cursor-pointer transition-colors relative group">
+            <div className="relative">
+              <ShoppingCart size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
+              <span className="absolute -top-2 -right-2 bg-primary text-white text-[10px] font-bold rounded-full w-4 h-4 flex items-center justify-center">2</span>
+            </div>
+            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Cart</p>
+          </div>
+
+          <div className="flex items-center cursor-pointer transition-colors group">
+            <CircleUserRound size={16} className="text-gray-500 group-hover:text-primary mr-1 transition-colors" />
+            <p className="text-xs text-gray-500 group-hover:text-primary transition-colors">Account</p>
+          </div>
+        </div>
+      </div>
+      {/* Navbar */}
+      <Navbar />
     </div>
   );
 };
